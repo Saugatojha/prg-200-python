fd41aaef4f921c0974fc10bdadededddf61d387f
Saugat Ojha <ojhasaugat81@gmail.com>
2026-03-25 08:44:36 +0545

    hcf/lcf using function and recursive function to find sum of n natural numbers

--- week3/function.py
+++ week3/function.py
@@ -1,8 +1,24 @@
-def odd_even(num):
-    if num % 2==0:
-        print("even")
-    else:
-        print("odd")
+# def odd_even(num):
+#     if num % 2==0:
+#         print("even")
+#     else:
+#         print("odd")
 
 
-odd_even(6)
+# odd_even(6)
+# // wap to find hcf and lcf using py
+def hcf(a, b):
+    while b != 0:
+        a, b = b, a % b  
+        # 12, 18 = 18, 6
+    return a
+# 108 // 18 , 6 
+def lcm(a, b):
+    return (a * b) // hcf(a, b)
+
+num1 = 12
+num2 = 18
+
+print("HCF:", hcf(num1, num2))
+print("LCM:", lcm(num1, num2))
+     
---
+++ week3/recursion.py
@@ -0,0 +1,7 @@
+
+def sum(n):
+    sum = n * (n + 1) // 2
+    return sum
+
+n = int(input("Enter a number"))
+print("sum", sum(n))
